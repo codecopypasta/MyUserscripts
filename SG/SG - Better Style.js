1188370766717ffc4bd0ced4e025baf9e12d73ad
codecopypasta <100680428+codecopypasta@users.noreply.github.com>
2022-06-28 11:43:44 +0530

    Create SG - Better Style.js

---
+++ SG/SG - Better Style.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         SG - Better Style
+// @version      1.0
+// @description  Imrpove the Style for SG as per my personal liking
+// @author       codecopypasta
+// @match        https://www.steamgifts.com
+// @match        https://www.steamgifts.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=steamgifts.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+	// Global CSS
+	$("body").append(`
+		<style type="text/css">
+			/*Delete Button*/
+			.sidebar__entry-delete {
+				background-image: linear-gradient(#900 0%, #963 100%) !important;
+			}
+
+			/*Entered GAs*/
+			.is-faded {
+				opacity: .25 !important;
+			}
+
+			a.giveaway__heading__name {
+				color: #4B72D4;
+			}
+
+			a:visited.giveaway__heading__name, a:visited.homepage_table_column_heading,
+			a:visited.table__column__heading, .comment__description a:visited {
+				/*color: #c99;*/
+				color: purple;
+			}
+		</style>
+	`);
+
+})();
