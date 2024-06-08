62de2bf86bf53b8c1f97befac7a89e42abed954c
codecopypasta <100680428+codecopypasta@users.noreply.github.com>
2024-06-08 12:01:24 +0530

    updated version

--- SG/SG - Bulk Action.js
+++ SG/SG - Bulk Action.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         SG - Bulk Action & Better GA listings
-// @version      2.9.6
+// @version      2.9.7
 // @description  Make SG easier to use
 // @author       codecopypasta
 // @match        https://www.steamgifts.com
